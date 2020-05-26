29c23b9bbba89ce58a101ad84d59be7f89b14bb0
donghwan.yu <koreacio@gmail.com>
2020-05-27 06:39:31 +0900

    [ch04] add some files

--- ch02_Variables/src/com/yudong80/java/ch02/MultiCalculatorV1.java
+++
@@ -1,7 +0,0 @@
-package com.yudong80.java.ch02;
-
-public class MultiCalculatorV1 {
-	public static void main(String[] args) { 
-		
-	}
-}
---
+++ ch04_FunctionString/src/com/yudong80/java/ch04/InputArgs.java
@@ -0,0 +1,9 @@
+package com.yudong80.java.ch04;
+
+public class InputArgs {
+	public static void main(String[] args) {
+		String name = args[0];
+		String city = args[1];	
+		System.out.println(city + "¿¡ »ç´Â " + name + "´Ô ¹Ý°©½À´Ï´Ù!");
+	}	
+}
