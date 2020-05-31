a70d1b62d679437265c089924aa82bdc5b392cc1
donghwan.yu <koreacio@gmail.com>
2020-05-31 10:13:21 +0900

    [ch04] add some files

--- ch04_FunctionString/src/com/yudong80/java/ch04/FibonacciV2.java
+++ ch04_FunctionString/src/com/yudong80/java/ch04/FibonacciV2.java
@@ -1,15 +1,20 @@
-package com.yudong80.java.ch03;
+package com.yudong80.java.ch04;
 
 import java.util.Scanner;
 
-public class ForFibonacci {
-	public static void main(String[] args) {
-		System.out.println("파보나치 수열 만들기");
-		
+public class FibonacciV2 {
+	static int getUserInput() {
 		Scanner s = new Scanner(System.in);	
 		System.out.println("수열 개수를 입력하세요");
 		int num = s.nextInt();
 		s.close(); //사용하면 최대한 빨리 닫아 줍니다 
+		return num;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("파보나치 수열 만들기");
+		
+		int num = getUserInput();
 		
 		int a = 1;
 		int b = 1;
---
+++ ch04_FunctionString/src/com/yudong80/java/ch04/StringChar.java
@@ -0,0 +1,7 @@
+package com.yudong80.java.ch04;
+
+public class StringChar {
+	public static void main(String[] args) {
+		
+	}
+}
