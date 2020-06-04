c86d2464d0defe68e45a115e08cf58736a0e74c4
donghwan.yu <koreacio@gmail.com>
2020-06-04 21:15:39 +0900

    [ch04] add some examples

--- ch04_FunctionString/src/com/yudong80/java/prj/MyStorageV2.java
+++ ch04_FunctionString/src/com/yudong80/java/prj/MyStorageV2.java
@@ -2,7 +2,10 @@ package com.yudong80.java.prj;
 
 import java.util.Scanner;
 
-public class MyStorageV1 {
+public class MyStorageV2 {
+	static String EMPTY = "없음";
+	static int EMPTY_COUNT = 0;
+	
 	static void showMenu() { 
 		System.out.println("MyStorage v1");
 		System.out.println("--------------------------");
@@ -20,8 +23,37 @@ public class MyStorageV1 {
 		return select;
 	}
 	
+	static void register(Scanner s, String[] products) {
+		System.out.println("새로운 물건을 등록합니다");
+		System.out.println("원하는 메뉴를 선택하세요? ");
+		
+		String input = s.next();
+		int emptyIdx = -1; //not empty
+		for (int i=0; i< products.length; ++i) {
+			String product = products[i];
+			if (EMPTY.equals(product)) {
+				emptyIdx = i;
+				break;
+			}
+		}
+		
+		if (emptyIdx < 0) {
+			System.out.println("창고가 가득찼습니다. 등록이 불가합니다.");
+			return;
+		}
+		
+		products[emptyIdx] = input;
+		System.out.println("등록이 완료되었습니다. 제품 목록은: ");
+		for (String product: products) {
+			System.out.println(product);
+		}
+	}
+	
 	public static void main(String[] args) {	
 		Scanner s = new Scanner(System.in);
+		String[] products = { EMPTY, EMPTY, EMPTY, EMPTY, EMPTY };
+		int[] counts = { 0, 0, 0, 0, 0 };
+		
 		while(true) {
 			//1. 메뉴를 표시한다 
 			showMenu();
@@ -35,16 +67,16 @@ public class MyStorageV1 {
 			
 			switch(menu) {
 			case 1: //register
-				System.out.println("물건을 등록합니다 <작업중>\n");
+				register(s, products);
 				break;
 			case 2: //deposit 
-				System.out.println("창고에 물건을 넣습니다 <작업중>\n");
+				System.out.println("창고에 물건을 넣습니다 <작업중>");
 				break;
 			case 3: //withdraw
-				System.out.println("창고에서 물건을 가져옵니다 <작업중>\n");
+				System.out.println("창고에서 물건을 가져옵니다 <작업중>");
 				break;
 			case 4: //search 
-				System.out.println("창고의 물건을 조회합니다 <작업중>\n");				
+				System.out.println("창고의 물건을 조회합니다 <작업중>");				
 				break;
 			}
 		}
