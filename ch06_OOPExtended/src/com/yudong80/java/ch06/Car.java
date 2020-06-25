d3ff86cd8fa66c6ab12937ca7eedc9a94e0db402
donghwan.yu <koreacio@gmail.com>
2020-06-25 23:18:55 +0900

    [ch06] add car examples

---
+++ ch06_OOPExtended/src/com/yudong80/java/ch06/Car.java
@@ -0,0 +1,18 @@
+package com.yudong80.java.ch06;
+
+/* 생략된 형태 
+ * interface Car {
+	void start();
+	void accelerate();
+	void accelerate(int km);
+	void decelerateHalf();
+	void decelerateAs(int km);
+}*/
+
+public interface Car {
+	public void start();
+	public void accelerate();
+	public void accelerate(int km);
+	public void decelerateHalf();
+	public void decelerateAs(int km);
+}
